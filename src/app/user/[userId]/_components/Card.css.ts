b46235c7d9cda946eadb2527923fe7fa63acd4ca
Sohyun Park <124856726+ParkSohyunee@users.noreply.github.com>
2024-08-04 13:52:23 +0900

    Feat: 팝업 옵션 메뉴 구현

--- src/app/user/[userId]/_components/Card.css.ts
+++ src/app/user/[userId]/_components/Card.css.ts
@@ -43,6 +43,19 @@ export const container = style({
   },
 });
 
+export const optionMenu = style({
+  padding: '1.6rem',
+  position: 'absolute',
+  right: 0,
+
+  display: 'flex',
+  flexDirection: 'column',
+
+  borderRadius: '1.2rem',
+  background: vars.color.white,
+  boxShadow: '0px 0px 4px 0px rgba(180, 180, 180, 0.04), 0px 8px 16px 0px rgba(136, 136, 136, 0.08)',
+});
+
 export const title = style({
   padding: '1.2rem 0 2rem 0',
 
--- src/app/user/[userId]/_components/Card.tsx
+++ src/app/user/[userId]/_components/Card.tsx
@@ -1,10 +1,14 @@
 import Image from 'next/image';
+import { MouseEvent } from 'react';
 import { assignInlineVars } from '@vanilla-extract/dynamic';
 
 import * as styles from './Card.css';
+import LockIcon from '/public/icons/lock_alt.svg';
 
 import useMoveToPage from '@/hooks/useMoveToPage';
-import LockIcon from '/public/icons/lock_alt.svg';
+import useBooleanOutput from '@/hooks/useBooleanOutput';
+import useOnClickOutside from '@/hooks/useOnClickOutside';
+
 import { ListType } from '@/lib/types/listType';
 import { BACKGROUND_COLOR_READ } from '@/styles/Color';
 
@@ -16,7 +20,16 @@ interface CardProps {
 
 export default function Card({ list, isOwner }: CardProps) {
   const { onClickMoveToPage } = useMoveToPage();
+  const { isOn, handleSetOff, handleSetOn } = useBooleanOutput();
+  const { ref } = useOnClickOutside(() => {
+    handleSetOff();
+  });
   const isVisibleLockIcon = isOwner && !list.isPublic;
+
+  const handleVisibleOption = (e: MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    handleSetOn();
+  };
 
   return (
     // MasonryGrid 라이브러리에서는 ul로 감싸줘야 하므로 Link태그 미사용
@@ -27,6 +40,11 @@ export default function Card({ list, isOwner }: CardProps) {
         [styles.listColor]: `${BACKGROUND_COLOR_READ[list.backgroundColor as keyof typeof BACKGROUND_COLOR_READ]}`,
       })}
     >
+      {isOn && (
+        <div ref={ref} className={styles.optionMenu}>
+          <span>비공개</span>
+        </div>
+      )}
       <div className={`${isVisibleLockIcon ? styles.listInfoPrivate : styles.listInfo}`}>
         {isVisibleLockIcon && (
           <div className={styles.lockIcon}>
@@ -34,7 +52,7 @@ export default function Card({ list, isOwner }: CardProps) {
             <LockIcon alt="비공개 리스트 표시" />
           </div>
         )}
-        <button className={styles.optionButton}>
+        <button className={styles.optionButton} onClick={handleVisibleOption}>
           <Image src="/icons/more_option_button.svg" alt="더보기 버튼" width={2} height={10} />
         </button>
       </div>
